f8da637566616fd3464640634100d4f298a77f69
Rajesh jaga <Rajaga223@gmail.com>
2020-09-30 09:42:49 +0530

    style sheets seperated and file names changed

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,24 @@
   <div id="app">
     <h3 class="app-title">
       Made By
-      <a href="http://www.github.com/rajeshjaga" class="github" _target="blank">Lucifer__13</a>
+      <a href="http://www.github.com/rajeshjaga" class="github" _target="blank"
+        >Lucifer__13</a
+      >
     </h3>
-    <Temo />
+    <Darkmode />
   </div>
 </template>
 
 <script>
-import Temo from "./components/Temo.vue";
+import Darkmode from "./components/Darkmode.vue";
 export default {
   name: "App",
   components: {
-    Temo
-  }
+    Darkmode,
+  },
 };
 </script>
 
 <style>
-*,
-*:after,
-*:before {
-  margin: 0;
-  padding: 0;
-}
-.app-title {
-  font-family: "Playfair Display", serif;
-  position: absolute;
-  bottom: 2em;
-  right:1em;
-}
-.github {
-  text-decoration: none;
-  color: rgb(19, 189, 189);
-}
+@import "./stylesheets/globals.scss";
 </style>
---
+++ src/components/Darkmode.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Darkmode">
+    <input type="checkbox" name="checkbox" id="checkbox" />
+    <div class="color">
+      <label for="checkbox">
+        <i class="far fa-sun"></i>
+        <i class="far fa-moon"></i>
+        <div class="pointer"></div>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Darkmode",
+};
+</script>
+
+<style scoped >
+@import "../stylesheets/darkmode.scss";
+</style>
